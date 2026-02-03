a4135d89d101c6a4ed587fb77549692783356649
ibroh-tech <ibrohimmurodjonov515@gmail.com>
2026-02-03 19:38:46 +0300

    Integration testing

--- src/test/java/com/example/demo/integration/config/AbstractIntegrationTest.java
+++ src/test/java/com/example/demo/integration/config/AbstractIntegrationTest.java
@@ -1,4 +1,24 @@
 package com.example.demo.integration.config;
 
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
 public class AbstractIntegrationTest {
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18.1");
+
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 }
