d049dd69262b67a0490f02038fd95e4441595973
Ibrohim Murodjonov <ibrohimmurodjonov515@example.com>
2026-02-16 10:50:13 +0500

    Redis container is tested

---
+++ src/test/java/com/example/demo/integration/config/RedisContainerConfiguration.java
@@ -0,0 +1,46 @@
+package com.example.demo.integration.config;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+public class RedisContainerConfiguration {
+
+    @Container
+    static GenericContainer<?> redis =
+            new GenericContainer<>("redis:7-alpine")
+                    .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port",
+                () -> redis.getMappedPort(6379));
+    }
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Test
+    void shouldStoreAndRetrieveValue() {
+        redisTemplate.opsForValue().set("key", "value");
+        String result = redisTemplate.opsForValue().get("key");
+
+        assertEquals("value", result);
+    }
+}
